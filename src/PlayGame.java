019a62216150d771e2cd111c436afe2111b1c0f8
choonjerald <choonjerald@gmail.com>
2020-02-08 20:48:07 +0800

    playgame updated to take user input for number of players and name

--- src/PlayGame.java
+++ src/PlayGame.java
@@ -1,30 +1,83 @@
+import java.util.Scanner;
+
 public class PlayGame {
 
 	public static void main(String[] args) {
 		Pool P = new Pool();
 		ScrabblePlayer scrabblePlayers = new ScrabblePlayer(P);
-		
-		scrabblePlayers.addPlayer("John");
-		System.out.println(scrabblePlayers.players.get(0));
-		System.out.println("Tiles left in pool: " + P.tilesLeft());		
-		System.out.println(scrabblePlayers.players.get(0).getFrame());
+
+		int numOfPlayers = 0;
+		String playerName;
+
+		Scanner q = new Scanner(System.in);
+		System.out.println("Enter amount of players: ");
+		numOfPlayers = Integer.parseInt(q.nextLine());
+		while (numOfPlayers > 4 || numOfPlayers < 0){    //guard for min and max players, will keep asking until appropriate value is given
+			if(numOfPlayers > 4 || numOfPlayers < 0) {
+				System.out.println("# of players entered is invalid. Only 2 to 4 players allowed");
+				numOfPlayers = Integer.parseInt(q.nextLine());
+			}
+			else{
+				break;
+			}
+		}
+
+		// set name for players
+		for(int i = 0; i < numOfPlayers; i++){
+			System.out.println("Player " + i + 1 + " Enter your name: ");
+			playerName = q.nextLine();
+			scrabblePlayers.addPlayer(playerName.trim());
+		}
+
+		for(int i = 0; i < numOfPlayers; i++){
+			System.out.println(scrabblePlayers.players.get(i));
+			System.out.println(scrabblePlayers.players.get(i).getFrame());
+		}
+
+		System.out.println();
+		System.out.println("To show that removing a tile from frame, refilling frame, and number of tiles left in bag works appropriately:");
+		System.out.println("For Player 1");
+		System.out.println("Tiles left in pool: " + P.tilesLeft());
+		System.out.println("Removing a tile...");
 		scrabblePlayers.players.get(0).getFrame().removeTile(1);
 		System.out.println(scrabblePlayers.players.get(0).getFrame());
+		System.out.println("Refilling Frame...");
 		scrabblePlayers.players.get(0).getFrame().refillFrame();
 		System.out.println(scrabblePlayers.players.get(0).getFrame());
-		System.out.println("Tiles left in pool: " + P.tilesLeft());		
-		
-		scrabblePlayers.addPlayer("Bob");
-		System.out.println(scrabblePlayers.players.get(1));
-		System.out.println("Tiles left in pool: " + P.tilesLeft());		
-		System.out.println(scrabblePlayers.players.get(1).getFrame());
+		System.out.println("Tiles left in pool: " + P.tilesLeft());
+
+		System.out.println();
+		System.out.println("For Player 2");
+		System.out.println("Tiles left in pool: " + P.tilesLeft());
+		System.out.println("Removing a tile...");
 		scrabblePlayers.players.get(1).getFrame().removeTile(1);
 		System.out.println(scrabblePlayers.players.get(1).getFrame());
+		System.out.println("Refilling Frame...");
 		scrabblePlayers.players.get(1).getFrame().refillFrame();
 		System.out.println(scrabblePlayers.players.get(1).getFrame());
 		System.out.println("Tiles left in pool: " + P.tilesLeft());
-		scrabblePlayers.players.get(1).updateScore(100);
-		System.out.println(scrabblePlayers.players.get(1));
+
+//		scrabblePlayers.addPlayer("John");
+//		System.out.println(scrabblePlayers.players.get(0));
+//		System.out.println("Tiles left in pool: " + P.tilesLeft());
+//		System.out.println(scrabblePlayers.players.get(0).getFrame());
+//		scrabblePlayers.players.get(0).getFrame().removeTile(1);
+//		System.out.println(scrabblePlayers.players.get(0).getFrame());
+//		scrabblePlayers.players.get(0).getFrame().refillFrame();
+//		System.out.println(scrabblePlayers.players.get(0).getFrame());
+//		System.out.println("Tiles left in pool: " + P.tilesLeft());
+//
+//		scrabblePlayers.addPlayer("Bob");
+//		System.out.println(scrabblePlayers.players.get(1));
+//		System.out.println("Tiles left in pool: " + P.tilesLeft());
+//		System.out.println(scrabblePlayers.players.get(1).getFrame());
+//		scrabblePlayers.players.get(1).getFrame().removeTile(1);
+//		System.out.println(scrabblePlayers.players.get(1).getFrame());
+//		scrabblePlayers.players.get(1).getFrame().refillFrame();
+//		System.out.println(scrabblePlayers.players.get(1).getFrame());
+//		System.out.println("Tiles left in pool: " + P.tilesLeft());
+//		scrabblePlayers.players.get(1).updateScore(100);
+//		System.out.println(scrabblePlayers.players.get(1));
 	}
 
 }
