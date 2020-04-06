70e2e1a246886b871a60ab38f1bb5a9225d8ded0
shijieliu789 <shijie.liu@ucdconnect.ie>
2020-04-06 02:14:03 +0100

    implementation of insert function of word trie, and search function for challenge:)

--- src/logic/Dictionary.java
+++ src/logic/Dictionary.java
@@ -5,8 +5,20 @@ import java.util.Scanner;
 
 public class Dictionary {
 	
-	private final static File DICTIONARY_FILE = new File("src/Collins_Scrabble_Words_2019.txt"); 
-	
+	private final static File DICTIONARY_FILE = new File("src/Collins_Scrabble_Words_2019.txt");
+	static final int ALPHABET_SIZE = 26;
+
+	static final class TrieNode {
+		TrieNode[] children = new TrieNode[ALPHABET_SIZE];
+		boolean isEndOfWord;		//true if node represents end of word
+
+		TrieNode() {
+			isEndOfWord = false;
+			for (int i = 0; i < ALPHABET_SIZE; i++)
+				children[i] = null;
+		}
+	}
+
 	public Dictionary() throws Exception{
 		initDictionary();
 	}
@@ -18,19 +30,77 @@ public class Dictionary {
 		}
 		scanner.close();
 	}
-	
-	//Method inserts word into tree
-	public static void insert(String word){ 
+
+	static TrieNode root;
+
+	public static void insert(String word){ 	//inserts dictionary words into our trie
 		//needs to be implemented
+		int level, index;
+		int length = word.length();
+
+		TrieNode current = root;
+
+		for(level =0; level < length; level++){
+			index = word.charAt(level) - 'A';
+			if (current.children[index] == null) {
+				current.children[index] = new TrieNode();
+			}
+
+			current = current.children[index];
+		}
+
+		current.isEndOfWord = true;
 	}
-	
-	//Method returns true if word is in tree
-	public static boolean search(String word) { 
-		return Board.challengeWord();	//this needs to be deleted
+												//need to link this with challenge button :)
+	public static boolean search(String word) {		//returns trur if word is in trie
+		int level;
+		int length = word.length();
+		int index;
+		TrieNode current = root;
+		word = word.toUpperCase();
+
+		for (level = 0; level < length; level++)
+		{
+			index = word.charAt(level) - 'A';
+
+			if (current.children[index] == null)
+				return false;
+
+			current = current.children[index];
+		}
+
+		return (current != null && current.isEndOfWord);
+//		return Board.challengeWord();	//this needs to be deleted
 	}
 	
 	public static void main(String args[]) throws Exception { 
-		
-	} 
-	
+		root = new TrieNode();
+
+		initDictionary();
+		if(search("SUPEREFFICIENCY") == true) {
+			System.out.println("found");
+		}else{
+			System.out.println("not found");
+		}
+
+		if(search("RubbishText") == true) {
+			System.out.println("found 2");
+		}else{
+			System.out.println("not found 2");
+		}
+
+		if(search("caPItAl") == true) {
+			System.out.println("found 3");
+		}else{
+			System.out.println("not found 3");
+		}
+
+		if(search("Overwatch") == true) {
+			System.out.println("found 4");
+		}else{
+			System.out.println("not found 4");
+		}
+	}
+
+
 }
