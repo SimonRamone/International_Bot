f8cb40b539bb6dcad418444ff81a190aa121b94c
shijieliu789 <shijie.liu@ucdconnect.ie>
2020-04-17 13:40:08 +0100

    Dictionary for code

--- src/BOT/BasicBots_v2/Dictionary.java
+++ src/BOT/BasicBots_v2/Dictionary.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Dictionary implements DictionaryAPI {
 
-    private static String inputFileName = "csw.txt";
+    private static String inputFileName = "C:\\Users\\jackl\\IdeaProjects\\International-Bot\\src\\view\\Collins_Scrabble_Words_2019.txt";
     private Node root;
 
     Dictionary() throws FileNotFoundException {
